67b7911677f1f4d7bd176f3ba9da2db60da13a12
Brady Keeling <bradykeeling@gmail.com>
2024-02-14 14:59:44 -0600

    Remove uneeded comments

--- lib/anycable/nsq.rb
+++ lib/anycable/nsq.rb
@@ -4,15 +4,3 @@ module AnyCable
 end
 
 require "anycable/nsq/server"
-
-# GRPC example
-
-# AnyCable.server_builder = ->(config) {
-#   AnyCable.logger.info "gRPC version: #{::GRPC::VERSION}"
-
-#   ::GRPC.define_singleton_method(:logger) { AnyCable.logger } if config.log_grpc?
-
-#   params = config.to_grpc_params
-
-#   AnyCable::GRPC::Server.new(**params, host: config.rpc_host)
-# }
